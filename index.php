3d5a04a65751911cce1f48ccaf504fc6c153707e
jhmj4-6 <joshipura117@gmail.com>
2025-09-10 15:23:36 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Main Template File
+ * 
+ * 
+ * @package Aquila
+ * 
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aquila</title>
+</head>
+<body>
+    Hello Wordpress
+</body>
+</html>
